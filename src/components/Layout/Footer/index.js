aba337ac15e61d31cc3f24ac0c8855fcf2e03602
ftmkrtgz <salihagemici.123@gmail.com>
2024-09-26 19:46:49 +0200

    changed files

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import { FaCopyright } from "react-icons/fa";
 function Footer() {
   return (
-    <footer className="mx-2">
-      <span className="mx-2">
+    <footer>
+      <span className="px-2">
         <FaCopyright />
       </span>
       2024 eCom store | Designed by Fatma Kurtgozu
